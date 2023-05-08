8c185ea620bb2dc1b049cb986cc97c15cc82b9f8
Noah0224 <alexano2428@gmail.com>
2023-05-08 08:07:38 -0500

    Este es el trabajo final del Challege de OracleOne

--- script.js
+++ script.js
@@ -1,129 +1,56 @@
- var botonEncriptar = document.querySelector (".boton-encriptar");
- var botonDesencriptar = document.querySelector (".boton-desencriptar");
- var munheco = document.querySelector (".contenedormunheco");
- var textoADesencriptar = document.querySelector (".textoadesencriptar");
- var contenedorResultado = document.querySelector (".texto-resultado");
- 
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
 
- botonEncriptar.onclick = encriptar;
- botonDesencriptar.onclick = desencriptar;
+function btnEncriptar(){
 
- function encriptar()
- {
-    ocultarAdelante();
-
-    var cajatexto = recuperarTexto()
-    contenedorResultado.textContent = encriptarTexto(cajadetexto);
- }
-
- function desencriptar()
- {
-    ocultarAdelante();
-    var cajatexto = recuperarTexto()
-    contenedorResultado.textContent = desencriptarTexto(cajadetexto)
- }
-
- function recuperarTexto(){
-
-    var cajatexto = textoADesencriptar.value 
-    return cajatexto.value  
- }
-
- function ocultarAdelante(){
-
-    munheco.classList.add ("ocultar");
-    contenedorResultado.classList.add("ocultar");
- }
-
- function encriptarTexto (mensaje) {
-    var texto = mensaje;
-    var textoFinal ="";
-    
-    for(var i = 0; i<texto.length; i++) {
-        if(texto[i] == "a"){
-        textoFinal = textoFinal + "ai"
-    }
-
-    else if(texto[i] == "e"){
-
-        textoFinal = textoFinal + "enter"
-
-    }
-    
-    else if(texto[i] == "i"){
-        textoFinal = textoFinal + "imes"
-    }
-
-    else if (texto [i] == "o") {
-        textoFinal = textoFinal + "ober"
-
-    }
-
-    else if (texto [i] == "u")  {
-        textoFinal = textoFinal + "ufat"
-
-    }
-
-    else {
-        textoFinal = textoFinal + texto [i]
-    }
-
-    }
-
-    return textoFinal;
+  const textoEncriptado = encriptar(textArea.value)
+  mensaje.value = textoEncriptado
+  textArea.value = "";
+  mensaje.style.backgroundImage= "none"
 }
 
-function desencriptarTexto (mensaje) {
-    var texto = mensaje;
-    var textoFinal ="";
-    
-    for(var i = 0; i<texto.length; i++) {
-        if(texto[i] == "a"){
-        textoFinal = textoFinal + "a"
-        i = i+1;
-    }
-
-    else if(texto[i] == "e"){
-
-        textoFinal = textoFinal + "e"
-        i = i+4;
-
-    }
-    
-    else if(texto[i] == "i"){
-        textoFinal = textoFinal + "i"
-        i = i+3;
-    }
-
-    else if (texto [i] == "o") {
-        textoFinal = textoFinal + "o"
-        i = i+3;
-
-    }
-
-    else if (texto [i] == "u")  {
-        textoFinal = textoFinal + "u"
-        i = i+3;
-
-    }
-
-    else {
-        textoFinal = textoFinal + texto [i]
-    }
-
-    }
- 
-    return textoFinal;
+function btnCopiar(){
+  const texto = mensaje.value || textArea.value;
+  navigator.clipboard.writeText(texto)
+    .catch(err => alert(`¡No se pudo copiar "${texto}" en el portapapeles: ${err}`));
 }
 
-const botonCopiar = document.querySelector (".boton-copiar");
+function encriptar (stringEncriptar){
 
-    botonCopiar.addEventListener ("click", copiar = () => {
-    
-    var contenido = document.querySelector(".texto-resultado").textContent;
-    navigator.clipboard.writeText(contenido);
-    console.log ("hola")
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+   stringEncriptar = stringEncriptar.toLowerCase()
 
+   for(i = 0; i < matrizCodigo.length; i++){
 
+    if(stringEncriptar.includes(matrizCodigo[i][0])){
 
-    })
+        stringEncriptar =  stringEncriptar.replaceAll(matrizCodigo[i][0], matrizCodigo [i][1]) 
+      }
+   }
+
+   return stringEncriptar
+
+}
+
+function btnDesencriptar(){
+
+  const textoEncriptado = desencriptar(textArea.value)
+  mensaje.value = textoEncriptado
+  textArea.value = "";
+}
+
+function desencriptar (stringDesencriptar){
+
+  let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+ stringDesencriptar = stringDesencriptar.toLowerCase()
+
+ for(i = 0; i < matrizCodigo.length; i++){
+
+  if(stringDesencriptar.includes(matrizCodigo[i][0])){
+
+      stringDesencriptar =  stringDesencriptar.replaceAll(matrizCodigo [i][1], matrizCodigo[i][0]) 
+ }
+
+ }
+ return stringDesencriptar
+}
